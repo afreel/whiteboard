b2fdea5edf632f5450556487433eaebd94042ba9
Michael Handley <handley@mit.edu>
2013-12-04 11:34:14 -0500

    Started work on WhiteboardServer

--- src/canvas/WhiteboardServer.java
+++ src/canvas/WhiteboardServer.java
@@ -4,10 +4,10 @@ import java.util.HashMap;
 
 public class WhiteboardServer {
 
-	private final HashMap<Integer, Whiteboard> whiteBoards;
+	private final HashMap<Integer, Whiteboard> whiteboardMap;
 	
 	public WhiteboardServer() {
-		//TODO:
+		this.whiteboardMap = new HashMap<Integer, Whiteboard>() {{ put(1, new Whiteboard()); put(2, new Whiteboard()); }}; 
 	}
 	public static void main(String[] args) {
 		
