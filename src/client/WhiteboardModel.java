5659ada132e115f90f1ab5695074015012cfb81c
Johannes Norheim <norheim@mit.edu>
2013-12-05 12:46:39 -0500

    Almost automised testing suite, generated methods for sending messages following the protocil to the server

--- src/client/WhiteboardModel.java
+++ src/client/WhiteboardModel.java
@@ -30,37 +30,58 @@ public class WhiteboardModel {
             in = new BufferedReader(new InputStreamReader(
                     socket.getInputStream()));
         } catch (IOException e1) {
+            System.out
+                    .println("Couldnt connect to " + host + " @ port " + port);
             e1.printStackTrace();
         }
 
-        new Thread(new Runnable() {
-            public void run() {
-                // Have a loop constantly checking for new messages from the
-                // server.
-                try {
-                    String inputLine;
-                    while ((inputLine = in.readLine()) != null) {
-                        System.out
-                                .println("Received a message from the server: "
-                                        + inputLine);
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace(); // but don't terminate server
-                } finally {
-                    try {
-                        // Close connection with server
-                        System.out.println("Closing connection with server");
-                        socket.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }).start();
+        // Have a thread constantly listen for server messages
+        new Thread(new serverListener()).start();
 
     }
 
     public void sendMessageToServer(String message) {
         out.println(message);
+    }
+
+    public void connectToWhiteBoard(String whiteboard, String username) {
+        out.println("whiteboard " + whiteboard + "username" + username);
+    }
+
+    public void drawLineOnServer(int x1, int y1, int x2, int y2, int width,
+            int r, int g, int b) {
+        out.println("line " + Integer.toString(x1) + " " + Integer.toString(y1)
+                + " " + Integer.toString(x2) + " " + Integer.toString(y2) + " "
+                + Integer.toString(width) + " " + Integer.toString(r) + " "
+                + Integer.toString(g) + " " + Integer.toString(b));
+    }
+
+    /**
+     * serverListener is a functor that will be used for spinning a new Thread
+     * listening for messages sent from the server over the socket.
+     */
+    private class serverListener implements Runnable {
+        @Override
+        public void run() {
+            // Have a loop constantly checking for new messages from the
+            // server.
+            try {
+                String inputLine;
+                while ((inputLine = in.readLine()) != null) {
+                    System.out.println("Client just got from the server: '"
+                            + inputLine + "'");
+                }
+            } catch (IOException e) {
+                e.printStackTrace(); // but don't terminate server
+            } finally {
+                try {
+                    // Close connection with server
+                    System.out.println("Closing connection with server");
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
--- src/client/WhiteboardModelTest.java
+++ src/client/WhiteboardModelTest.java
@@ -1,6 +1,7 @@
 package client;
 
 import java.net.*;
+import java.util.List;
 import java.io.*;
 
 import static org.junit.Assert.*;
@@ -8,51 +9,77 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class WhiteboardModelTest {
+    private List<String> clientSentMessages;
+    private List<String> clientReceivedMessages;
+    private List<String> serverSentMessages;
+    private List<String> serverReceivedMessages;
 
     @Test
     public void serverGetsMessage() throws IOException {
+        String message1 = "HI";
+        String message2 = "HI again";
         
-        Thread server = new Thread(new Runnable() {
-            public void run() {
-                int portNumber = 4444;
-                PrintWriter out;
-                BufferedReader in;
-                               
-                try {
-                    @SuppressWarnings("resource")
-                    ServerSocket serverSocket = new ServerSocket(portNumber);
-                    Socket clientSocket = serverSocket.accept();
-                    
-                    out = new PrintWriter(clientSocket.getOutputStream(), true);
-                    in = new BufferedReader(new InputStreamReader(
-                            clientSocket.getInputStream()));
-                    
-                    String inputLine;
-                    while ((inputLine = in.readLine()) != null) {
-                        // Echo the message back
-                        System.out.println("Recieved message from client: '"+inputLine+"'");
-                        out.println(inputLine);
-                    }
-                    
-                } catch (IOException e) {
-                    System.out
-                    .println("Exception caught when trying to listen on port "
-                            + portNumber + " or listening for a connection");
-            System.out.println(e.getMessage());
+        new Thread(new echoServer()).start();
+
+        WhiteboardModel whitemodel = new WhiteboardModel("localhost",
+                4444);
+        
+        clientSentMessages.add(message1);
+        whitemodel.sendMessageToServer(message1);
+        
+        clientSentMessages.add(message2);
+        whitemodel.sendMessageToServer(message2);
+
+        try {
+            // We need to make the algorithm wait so we can visually check that
+            // the GUI is correct
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Generates a server that prints out whichever message it gets and echoes
+     * the same message back.
+     */
+    public class echoServer implements Runnable {
+        @Override
+        public void run() {
+            int portNumber = 4444;
+            PrintWriter out;
+            BufferedReader in;
+
+            try {
+                @SuppressWarnings("resource")
+                ServerSocket serverSocket = new ServerSocket(portNumber);
+                System.out.println("Server started");
+                
+                Socket clientSocket = serverSocket.accept();
+                String client = clientSocket.getInetAddress().toString();
+                
+                System.out.println(client
+                        + " connected");
+
+                out = new PrintWriter(clientSocket.getOutputStream(), true);
+                in = new BufferedReader(new InputStreamReader(
+                        clientSocket.getInputStream()));
+
+                String inputLine;
+                while ((inputLine = in.readLine()) != null) {
+                    serverReceivedMessages.add("Server recieved: '" + inputLine + "'");
+                    serverSentMessages.add("Server echoing '" + inputLine
+                            + "' back to client");
+                    // Echo the message back
+                    out.println(inputLine);
                 }
 
+            } catch (IOException e) {
+                System.out
+                        .println("Exception caught when trying to listen on port "
+                                + portNumber + " or listening for a connection");
+                System.out.println(e.getMessage());
             }
-        });
-        
-        Thread client = new Thread(new Runnable(){
-            @Override
-            public void run() {
-                WhiteboardModel whitemodel = new WhiteboardModel("localhost",4444);
-                whitemodel.sendMessageToServer("HI");
-            }
-        });
-        
-        server.start();
-        client.start();
+        }
     }
 }
