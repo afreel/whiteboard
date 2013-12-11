1c56fac7493732b75a187e9ef84c4cecf61237e3
Johannes Norheim <norheim@mit.edu>
2013-12-11 15:53:38 -0500

    Added test for whiteboard correctly distributing the message to all clients

--- src/server/WhiteboardTest.java
+++ src/server/WhiteboardTest.java
@@ -3,7 +3,6 @@ package server;
 import static org.junit.Assert.*;
 
 import java.io.IOException;
-import java.net.Socket;
 import testing.TestUtils;
 import org.junit.Test;
 
@@ -25,7 +24,8 @@ import org.junit.Test;
  */
 
 public class WhiteboardTest {
-
+    private String sampleLineMessage = "line 100 100 200 200 5 0 0 0";
+    
 	@Test
 	public void addClientBasicTest() {
 		try {
@@ -33,7 +33,10 @@ public class WhiteboardTest {
 			utils.startServer(4444);
 			Whiteboard board = new Whiteboard();
 			Client c = new Client("Test", utils.spawnClient("Test", "1", 4444));
+			
 			board.addClient(c);
+            utils.sleep();
+            
 			assertEquals(board.getClientsTesting().get(0), c);
 		}
 		catch (IOException e1) {}
@@ -45,10 +48,14 @@ public class WhiteboardTest {
 			TestUtils utils = new TestUtils();
 			utils.startServer(1234);
 			Whiteboard board = new Whiteboard();
-			Client c1 = new Client("Joe", utils.spawnClient("Test", "1", 1234));
-			Client c2 = new Client("Joe", utils.spawnClient("Test", "1", 1234));
+			Client c1 = new Client("Joe", utils.spawnClient("Joe", "1", 1234));
+			Client c2 = new Client("Joe", utils.spawnClient("Joe", "1", 1234));
+			
 			board.addClient(c1);
+            utils.sleep();
 			board.addClient(c2);
+            utils.sleep();
+            
 			assertEquals(board.getClientsTesting().get(0), c1);
 			assertEquals(board.getClientsTesting().get(1), c2);
 		} catch (IOException e) {}
@@ -57,16 +64,23 @@ public class WhiteboardTest {
 	@Test
 	public void removeClientBasicTest() {
 		try {
+		    System.out.println("------------------------------------");
 			TestUtils utils = new TestUtils();
 			utils.startServer(3000);
 			Whiteboard board = new Whiteboard();
-			Client c1 = new Client("Mike", utils.spawnClient("Test", "1", 3000));
-			Client c2 = new Client("Austin", utils.spawnClient("Test", "1", 3000));
+			Client c1 = new Client("Mike", utils.spawnClient("Mike", "1", 3000));
+			Client c2 = new Client("Austin", utils.spawnClient("Austin", "1", 3000));
+			
 			board.addClient(c1);
+            utils.sleep();
 			board.addClient(c2);
+            utils.sleep();
 			board.removeClient(c1);
+			utils.sleep();
+			
 			assertEquals(board.getClientsTesting().size(), 1);
 			assertEquals(board.getClientsTesting().get(0), c2);
+			System.out.println("------------------------------------");
 		} catch (IOException e) {}
 	}
 	
@@ -76,15 +90,69 @@ public class WhiteboardTest {
 			TestUtils utils = new TestUtils();
 			utils.startServer(3333);
 			Whiteboard board = new Whiteboard();
-			Client c1 = new Client("Johannes", utils.spawnClient("Test", "1", 3333));
-			Client c2 = new Client("Joe", utils.spawnClient("Test", "1", 3333));
+			Client c1 = new Client("Johannes", utils.spawnClient("Johannes", "1", 3333));
+			Client c2 = new Client("Joe", utils.spawnClient("Joe", "1", 3333));
+			
 			board.addClient(c1);
+            utils.sleep();
 			board.removeClient(c2);
+            utils.sleep();
+            
 			assertEquals(board.getClientsTesting().size(), 1);
 			assertEquals(board.getClientsTesting().get(0), c1);
+			
 		} catch (IOException e) {}
 	}
 	
+   @Test
+    public void successfullySendsMessageToOneClient() {
+        try {
+            TestUtils utils = new TestUtils();
+            utils.startServer(4445);
+            Whiteboard board = new Whiteboard();
+            Client c1 = new Client("Johannes", utils.spawnClient("Johanens", "1", 4445));
+            
+            board.addClient(c1);
+            utils.sleep();
+            board.addLine(sampleLineMessage);
+            utils.sleep();   
+            
+            String expectedMessage = "[users Johannes, "+sampleLineMessage+"]";
+            
+            assertEquals(board.getClientsTesting().size(), utils.clientReceivedMessages.size());
+            assertEquals(expectedMessage, utils.clientReceivedMessages.get(0).toString());
+            
+        } catch (IOException e) {}
+    }
+	    
+   @Test
+   public void successfullySendsMessageToManyClients() {
+       try {
+           TestUtils utils = new TestUtils();
+           utils.startServer(4446);
+           Whiteboard board = new Whiteboard();
+           
+           Client c1 = new Client("Johannes", utils.spawnClient("Johanens", "1", 4446));
+           Client c2 = new Client("Joe", utils.spawnClient("Joe", "1", 4446));
+           Client c3 = new Client("Austin", utils.spawnClient("Austin", "1", 4446));
+           Client c4 = new Client("Mike", utils.spawnClient("Mike", "1", 4446));
+           
+           board.addClient(c1);
+           utils.sleep();
+           board.addClient(c2);
+           utils.sleep();
+           board.addClient(c3);
+           utils.sleep();
+           board.addClient(c4);
+           utils.sleep();
+           board.addLine(sampleLineMessage);
+           utils.sleep();            
+           
+           assertEquals(board.getClientsTesting().size(), utils.clientReceivedMessages.size());
+           
+       } catch (IOException e) {}
+   }
+   
 	@Test
 	public void addLineBasicTest() {
 		Whiteboard board = new Whiteboard();
